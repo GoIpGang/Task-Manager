e2f861546705ce8b9ef8112384d968836f22714a
BuildTools <unconfigured@null.spigotmc.org>
2017-12-23 21:14:06 +0100

    + main form

---
+++ src/gui/MainForm.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+
+public class MainForm {
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
